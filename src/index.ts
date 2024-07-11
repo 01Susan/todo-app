4a3cffc863492d6d3951e11201c2c515c0965435
01Susan <dhitalsusan2@gmail.com>
2024-07-11 08:35:23 +0545

    Added Cors

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { userRoute } from './routes/user'
 import { todoRoute } from './routes/todo'
+import cors from 'cors'
 dotenv.config()
 
 export const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use("/user", userRoute)
